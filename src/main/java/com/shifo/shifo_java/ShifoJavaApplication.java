9c64b2e55406fe2e35353098f4b6d2fbd877b1b0
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-11-27 12:22:37 -0500

    Initial commit

---
+++ src/main/java/com/shifo/shifo_java/ShifoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.shifo.shifo_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShifoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShifoJavaApplication.class, args);
+	}
+
+}
