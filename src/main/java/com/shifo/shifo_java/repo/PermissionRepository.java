4a2c35e3a0b5901351a4e5972dea38bbb3d2234d
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2026-02-14 14:56:40 -0500

    refactor: Remove outdated analytics and appointments modules

--- src/main/java/com/shifo/shifo_java/repo/PermissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shifo.shifo_java.repo;
-
-import com.shifo.shifo_java.features.permission.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermissionRepository extends JpaRepository<Permission, Long> {
-}
